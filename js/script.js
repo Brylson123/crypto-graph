112bbe6d9350d9ce9ad99a20d715c899fed3f4e8
Brylson123 <75631255+Brylson123@users.noreply.github.com>
2021-11-08 19:01:19 +0100

    change switch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
 
- let graph = document.getElementsByClassName("ccc-widget ccc-chart")
+const graph = document.getElementsByClassName("ccc-widget ccc-chart")
+const select = document.querySelector("select")
 
-function change() {
+// select.addEventListener("click",()=>{
+    // switch (select.value){
+    // case Btc:
+    //     $(graph).remove()
+    //     baseUrl = "https://widgets.cryptocompare.com/";
+    //     var scripts = document.getElementsByTagName("script");
+    //     var embedder = scripts[scripts.length - 1];
+    //     (function () {
+    //         var appName = encodeURIComponent(window.location.hostname);
+    //         if (appName == "") { appName = "local"; }
+    //         var s = document.createElement("script");
+    //         s.type = "text/javascript";
+    //         s.async = true;
+    //         var theUrl = baseUrl + 'serve/v1/coin/chart?fsym=BTC&tsym=USD';
+    //         s.src = theUrl + (theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+    //         document.getElementById("boxc").appendChild(s);
+    //     })();
+    //     break;
+    // case Eth:{
+    //             $(graph).remove()
+    //     baseUrl = "https://widgets.cryptocompare.com/";
+    //     var scripts = document.getElementsByTagName("script");
+    //     var embedder = scripts[scripts.length - 1];
+    //     var cccTheme = { "Followers": { "color": "#123" } };
+    //     (function () {
+    //         var appName = encodeURIComponent(window.location.hostname);
+    //         if (appName == "") { appName = "local"; }
+    //         var s = document.createElement("script");
+    //         s.type = "text/javascript";
+    //         s.async = true;
+    //         var theUrl = baseUrl + 'serve/v1/coin/chart?fsym=ETH&tsym=USD';
+    //         s.src = theUrl + (theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+    //         document.getElementById("boxc").appendChild(s);
+    //     })();
+
+    // }
+
+
+// 
+
+const change =()=> {
     if (document.getElementById('Cryselect').value == "Btc") {
         $(graph).remove()
         baseUrl = "https://widgets.cryptocompare.com/";
